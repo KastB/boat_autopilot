fac1f4907cbe3511769b01d83be3a679cf113473
Kast Bernd <kastbernd@gmx.de>
2017-06-01 20:59:24 +0200

    data: preprocessing for filtered yaw added

---
+++ data/correctAngle.m
@@ -0,0 +1,16 @@
+function  A = correctAngle(A, indexYaw)
+    for n = 2:size(A.data,1)
+
+        if A.data(n, indexYaw) -  A.data(n-1, indexYaw) > 180
+            A.data(n, indexYaw)
+            A.data(n, indexYaw) = A.data(n, indexYaw) - 360;
+            A.data(n, indexYaw)
+        end
+        if A.data(n, indexYaw) -  A.data(n-1, indexYaw) < -180
+            A.data(n, indexYaw)
+            A.data(n, indexYaw) = A.data(n, indexYaw) + 360;
+            A.data(n, indexYaw)
+         end
+
+    end
+end
--- data/preprocess.m
+++ data/preprocess.m
@@ -1,19 +1,6 @@
 A = importdata('~/src/autopilot_git/data/data.csv');
-indexYaw = 20;
-B = A
-for n = 2:size(A.data,1)
-    
-    if A.data(n, indexYaw) -  A.data(n-1, indexYaw) > 180
-        A.data(n, indexYaw)
-        A.data(n, indexYaw) = A.data(n, indexYaw) - 360;
-        A.data(n, indexYaw)
-    end
-    if A.data(n, indexYaw) -  A.data(n-1, indexYaw) < -180
-        A.data(n, indexYaw)
-        A.data(n, indexYaw) = A.data(n, indexYaw) + 360;
-        A.data(n, indexYaw)
-     end
-    
-end
-A
-plot(A.data(:,1), A.data(:,indexYaw),B.data(:,1), B.data(:,indexYaw))
+B = A;
+A = correctAngle(A,20);
+A = correctAngle(A,21);
+
+plot(   A.data(:,1), A.data(:,20), B.data(:,1), B.data(:,20),A.data(:,1), A.data(:,21), B.data(:,1), B.data(:,21))
