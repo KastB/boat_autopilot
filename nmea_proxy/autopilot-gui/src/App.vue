3e0f7e2d6195db50a942405742ca2c3d72021f00
Kast Bernd <kastbernd@gmx.de>
2022-06-12 11:52:46 +0200

    cleanup + precision

--- nmea_proxy/autopilot-gui/src/App.vue
+++ nmea_proxy/autopilot-gui/src/App.vue
@@ -16,13 +16,13 @@
     <b-row class="mb-2">
       <b-col >  
           <b-button class="w-100" size="lg" @click="b_re_init" :disabled=b_disabled>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Re-init&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            Re-init
           </b-button>
       </b-col>
 
       <b-col >  
           <b-button  class="w-100" size="lg" @click="b_unlock">
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Entsperren&nbsp;&nbsp;&nbsp;&nbsp;
+            Entsperren
           </b-button>
       </b-col>
     </b-row>
@@ -36,30 +36,30 @@
 
     <b-row class="mb-2">
       <b-col ><h1 style="font-size: 2em;">Wind[°]:</h1></b-col>
-      <b-col ><h1 style="font-size: 2em;">{{data["m_wind.apparentAngle"]}}</h1></b-col>
+      <b-col ><h1 style="font-size: 2em;">{{parseFloat(data["m_wind.apparentangle"]).toFixed(2)}}</h1></b-col>
       <b-col ><h1 style="font-size: 2em;">Wind[kn]:</h1></b-col>
-      <b-col ><h1 style="font-size: 2em;">{{data.tws}}</h1></b-col>
+      <b-col ><h1 style="font-size: 2em;">{{parseFloat(data.tws).toFixed(2)}}</h1></b-col>
     </b-row>
 
     <b-row class="mb-2">
       <b-col ><h1 style="font-size: 2em;">Kurs[°]:</h1></b-col>
       <b-col ><h1 style="font-size: 2em;">{{data.yaw}}</h1></b-col>
       <b-col ><h1 style="font-size: 2em;">Fahrt[kn]:</h1></b-col>
-      <b-col ><h1 style="font-size: 2em;">{{data.gps_vel}}</h1></b-col>
+      <b-col ><h1 style="font-size: 2em;">{{parseFloat(data.gps_vel).toFixed(2)}}</h1></b-col>
     </b-row>
 
     <b-row class="mb-2">
       <b-col ><h1 style="font-size: 2em;">Temp[°C]:</h1></b-col>
-      <b-col ><h1 style="font-size: 2em;">{{data["m_speed.waterTemp"]}}</h1></b-col>
+      <b-col ><h1 style="font-size: 2em;">{{data["m_speed.watertemp"]}}</h1></b-col>
       <b-col ><h1 style="font-size: 2em;">Tiefe[m]:</h1></b-col>
-      <b-col ><h1 style="font-size: 2em;">{{data["m_depth.depthBelowTransductor"]}}</h1></b-col>
+      <b-col ><h1 style="font-size: 2em;">{{data["m_depth.depthbelowtransductor"]}}</h1></b-col>
     </b-row>
 
     <b-row class="mb-2">
       <b-col ><h1 style="font-size: 2em;">Ruder[tiks]:</h1></b-col>
-      <b-col ><h1 style="font-size: 2em;">{{data.CurrentPosition}}</h1></b-col>
+      <b-col ><h1 style="font-size: 2em;">{{data.currentposition}}</h1></b-col>
       <b-col ><h1 style="font-size: 2em;">Integral[°s]:</h1></b-col>
-      <b-col ><h1 style="font-size: 2em;">{{data.m_errorSum}}</h1></b-col>
+      <b-col ><h1 style="font-size: 2em;">{{data.m_errorsum}}</h1></b-col>
     </b-row>
 
     <b-row class="mb-2">
@@ -73,58 +73,43 @@
       <b-col ><h1 style="font-size: 2em;">Spannung[V]:</h1></b-col>
       <b-col ><h1 style="font-size: 2em;">{{data.m_voltage}}</h1></b-col>
       <b-col ><h1 style="font-size: 2em;">Strom[A]:</h1></b-col>
-      <b-col ><h1 style="font-size: 2em;">{{data.m_amperage}}</h1></b-col>
+      <b-col ><h1 style="font-size: 2em;">{{data.m_current}}</h1></b-col>
     </b-row>
 
 
     <b-row class="mb-2">
       <b-col ><h1 style="font-size: 2em;">VMG[kn]:</h1></b-col>
-      <b-col ><h1 style="font-size: 2em;">{{data.vmg}}</h1></b-col>
+      <b-col ><h1 style="font-size: 2em;">{{parseFloat(data.vmg).toFixed(2)}}</h1></b-col>
       <b-col ><h1 style="font-size: 2em;">Leistung[W]:</h1></b-col>
       <b-col ><h1 style="font-size: 2em;">{{data.m_power}}</h1></b-col>
     </b-row>
 
     <b-row class="mb-2">
       <b-col>  
-          <b-button class="w-100" size="lg" @click="b_pos">
-                  &nbsp;&nbsp;&nbsp;&nbsp;Position&nbsp;&nbsp;&nbsp;&nbsp;
-          </b-button>
+          <b-button class="w-100" size="lg" @click="b_pos" :disabled="data.m_goaltype == '0'">Position</b-button>
       </b-col>
 
       <b-col>  
-        <b-button class="w-100" size="lg" @click="b_komp">
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Kompass&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        </b-button>
+        <b-button class="w-100" size="lg" @click="b_komp" :disabled="data.m_goaltype == '2'">Kompass</b-button>
       </b-col>
       <b-col>  
-          <b-button class="w-100" size="lg" @click="b_wind">
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Wind&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          </b-button>
+          <b-button class="w-100" size="lg" @click="b_wind"  :disabled="data.m_goaltype == '1'">Wind</b-button>
       </b-col>
     </b-row>
 
 
     <b-row class="mb-2">
       <b-col>
-          <b-button class="w-100" size="lg"  @click="b_m10">
-                  &nbsp;&nbsp;&nbsp;&nbsp;-10&nbsp;&nbsp;&nbsp;&nbsp;
-          </b-button>
-      </b-col>
-
-      <b-col>  
-        <b-button class="w-100" size="lg" @click="b_m1">
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-1&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        </b-button>
+          <b-button class="w-100" size="lg"  @click="b_m10">-10</b-button>
       </b-col>
       <b-col>  
-          <b-button class="w-100" size="lg" @click="b_p1">
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+1&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          </b-button>
+        <b-button class="w-100" size="lg" @click="b_m1">-1</b-button>
       </b-col>
       <b-col>  
-          <b-button class="w-100" size="lg" @click="b_p10">
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+10&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          </b-button>
+          <b-button class="w-100" size="lg" @click="b_p1">+1</b-button>
+      </b-col>
+      <b-col>  
+          <b-button class="w-100" size="lg" @click="b_p10">+10</b-button>
       </b-col>
 </b-row>
 
@@ -176,22 +161,22 @@ export default {
       this.$socket.sendObj({cmds: ["P"]})
     },
     b_komp() {
-      this.$socket.sendObj({cmds: ["K"]})
+      this.$socket.sendObj({cmds: ["M"]})
     },
     b_wind() {
       this.$socket.sendObj({cmds: ["W"]})
     },
     b_m10() {
-      this.$socket.sendObj({cmds: ["-10"]})
+      this.$socket.sendObj({cmds: ["D10"]})
     },
     b_m1() {
-      this.$socket.sendObj({cmds: ["-1"]})
+      this.$socket.sendObj({cmds: ["D1"]})
     },
     b_p1() {
-      this.$socket.sendObj({cmds: ["+1"]})
+      this.$socket.sendObj({cmds: ["I1"]})
     },
     b_p10() {
-      this.$socket.sendObj({cmds: ["+10"]})
+      this.$socket.sendObj({cmds: ["I10"]})
     },
     b_unlock() {
       this.b_disabled = false
